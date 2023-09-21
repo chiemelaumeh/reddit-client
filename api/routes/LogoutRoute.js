104a3394780e207e9fb2df51be0c5812097c50aa
Chiemela Umeh <chiemelaumeh1@gmail.com>
2023-09-21 01:17:58 -0500

    Delete api directory

--- api/routes/LogoutRoute.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-const router = express.Router();
-router.get("/", (req, res) => {
-  res.clearCookie("token", "").send("Successfully logged out");
-  console.log("Successfully logged out");
-})
-export default router
